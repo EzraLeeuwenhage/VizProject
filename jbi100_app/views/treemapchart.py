eb4f7860ca58404d3e136121aefc27712244ccf3
ave-ottens <50297791+ave-ottens@users.noreply.github.com>
2022-01-30 20:39:44 +0100

    Treemap now using groupby. Also more robust on weird category combinations

--- jbi100_app/views/treemapchart.py
+++ jbi100_app/views/treemapchart.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import time
+import numpy as np
 
 from jbi100_app.views.shared import field_to_title
 
@@ -14,6 +15,7 @@ class TreeMapChart(html.Div):
         self.html_id = name.lower().replace(" ", "-")
         self.value_label = value_label
         self.feature_y = 'cost'
+        self.norms_calculated = False
 
         # Equivalent to `html.Div([...])`
         super().__init__(
@@ -27,27 +29,34 @@ class TreeMapChart(html.Div):
     # Value feature for colors, category feature for bars
     def update(self, df, category_features, normalize=False):
         print("start: updating plot")
+        cats = category_features
+        cats = [l for i, l in enumerate(cats) if l not in cats[:i] and not l is None]
+        if len(cats) == 0:
+            cats = ['casualty_severity']
+        category_features = cats
         start = time.time()
         if normalize:
             # a normalized version that makes the squares proportional to how often they are in df
             # i.e. make uncommon entries bigger, more common smaller
             # thus it is easier to see relative costs, removed from sample size
             # a large size tells you, GIVEN such a category occurred, it is relatively more costly
-            df['norm_factor'] = [1] * len(df)
-            for cat in category_features:
-                freqs = dict(df[cat].value_counts())
-                # scale up / down norm_cost depending on the right frequency
-                total = sum(freqs.values())
-                # expected number per class if they were uniformly distributed
-                expectation = 1 / len(freqs)
-                for key in freqs:
-                    ratio = freqs[key] / total
-                    # ratio * correction = expectation
-                    correction = expectation / ratio
-                    df.loc[df[cat] == key, 'norm_factor'] *= correction
-            df['norm_cost'] = df.apply(
-                lambda row: row['norm_factor'] * row[self.feature_y], axis=1)
-            df.loc[:, 'norm_cost'] /= df['norm_cost'].sum()
+            if not self.norms_calculated:
+                df['norm_factor'] = [1] * len(df)
+                for cat in category_features:
+                    freqs = dict(df[cat].value_counts())
+                    # scale up / down norm_cost depending on the right frequency
+                    total = sum(freqs.values())
+                    # expected number per class if they were uniformly distributed
+                    expectation = 1 / len(freqs)
+                    for key in freqs:
+                        ratio = freqs[key] / total
+                        # ratio * correction = expectation
+                        correction = expectation / ratio
+                        df.loc[df[cat] == key, 'norm_factor'] *= correction
+                df['norm_cost'] = df.apply(
+                    lambda row: row['norm_factor'] * row[self.feature_y], axis=1)
+                df.loc[:, 'norm_cost'] /= df['norm_cost'].sum()
+                self.norms_calculated = True
 
             # Color all the squares by the value of the last category so they are consistent
             # among buckets and between the pair of graphs
@@ -55,8 +64,14 @@ class TreeMapChart(html.Div):
             original_column = df[color]
             # Convert to a string to give it discrete colors
             df[color] = df[color].astype(str)
-            fig = px.treemap(df,
-                             path=[px.Constant("all")] + category_features,
+
+            cats = category_features
+            tree = df.groupby(cats)[cats + ['norm_cost']].agg(
+                {'norm_cost': np.sum}
+            ).reset_index()
+
+            fig = px.treemap(tree,
+                             path=[px.Constant("all")] + cats,
                              values='norm_cost',
                              color=color
                             ) 
@@ -66,12 +81,29 @@ class TreeMapChart(html.Div):
             original_column = df[color]
             # Convert to a string to give it discrete colors
             df[color] = df[color].astype(str)
+
+            cats = category_features
+            cats = [l for i, l in enumerate(cats) if l not in cats[:i]]
+            tree = df.groupby(cats)[cats + ['cost']].agg(
+                {'cost': np.sum}
+            ).reset_index()
+
+            fig = px.treemap(
+                tree,
+                values='cost',
+                path=[px.Constant("all")] + cats,
+                color=color
+            )
+            df[color] = original_column
+
+            """
             fig = px.treemap(df,
                              path=[px.Constant("all")] + category_features,
                              values=self.feature_y,
                              color=color
                             ) 
             df[color] = original_column
+            """
         fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
 
         print(f"done: updating {self.html_id} plot in {time.time() - start:.2f}s")
